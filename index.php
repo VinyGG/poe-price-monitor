4ae240300c7a5300fde9a829cd3090fb152176c4
VinyGG <viniciusalves@msn.com>
2024-08-07 13:09:45 -0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
 $json = file_get_contents('https://poe.ninja/api/data/itemoverview?league=Settlers&type=Scarab');
 $obj = json_decode($json);
 echo $obj;
 ?>
-
-</body>
-</html>
